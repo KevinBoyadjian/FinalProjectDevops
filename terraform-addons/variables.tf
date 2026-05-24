1873db63db586df4062f4a046251db1c26e07792
Junior developer <junior@start-up.com>
2026-05-24 10:37:04 +0300

    Adding project name pull from core to add-ons

--- terraform-addons/variables.tf
+++ terraform-addons/variables.tf
@@ -1,8 +1,3 @@
-variable "project_name" {
-  description = "The name of the project"
-  type        = string
-}
-
 variable "grafana_admin_password" {
   description = "The admin password for Grafana"
   type        = string
--- terraform-core/outputs.tf
+++ terraform-core/outputs.tf
@@ -1,3 +1,10 @@
+output "project_name" {
+  description = "The name of the project"
+  value       = var.project_name
+}
+
+
+
 output "cluster_name" {
     description = "The name of the EKS cluster"
     value       = module.eks_cluster.cluster_name
